d17444b6a1552aabd66276cf5f7ff17763e668c5
Wang Minkai <167325983+HRWellls@users.noreply.github.com>
2025-03-07 11:58:11 +0800

    the framework of lab1

---
+++ ai_chip_stu_exp1/code/core/VecDot.v
@@ -0,0 +1,18 @@
+` timescale 1ns / 1ps
+
+module Vdot(
+    input clk,                                        
+    input rst,
+    input EN,                                           
+    input[31:0] A, B,
+    output[31:0] res
+);
+
+// [Topic 1] Please finish the code of vector dot product that requires 3 cycles for execution.
+// You can use the Mult4 module in common/MULT4.v to implement the multiplication of 4-bit numbers.
+
+// You may add some code here...
+
+assign res = // Complete the signal here.
+
+endmodule
